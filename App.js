7669066449827815fedc5dc1a02592d772c762a8
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-02-16 00:13:46 -0600

    added a text input

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default class App extends React.Component {
+  state={
+    name: ""
+  }
+
+changeTextHandler = (value) => {
+  this.setState({name: value});
+}
   render() {
     return (
       <View style={styles.container}>
         <Text>Adrians first mobile app!</Text>
+        < TextInput
+        style={{ width: 300,borderColor: 'black', borderWidth:1}}
+        value={this.state.name}
+        onChangeText={this.changeTextHandler}
+
+        />
       </View>
     );
   }
