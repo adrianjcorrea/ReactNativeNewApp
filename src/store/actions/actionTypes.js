20e599326f2f1b247c151f4b628238026d4e5d8f
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-03-06 18:20:27 -0600

    added an action file called actionsType

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,5 @@
+//Setting ann action to a variable for easier handling
+export const DELETE_PLACE = "DELETE_PLACE";
+export const ADD_PLACE = "ADD_PLACE";
+export const SELECT_PLACE = "SELECT_PLACE";
+export const DESELECT_PLACE = "DESELECT_PLACE";
